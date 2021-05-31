b9a258957e78bb523da9aae31092947689e16470
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-05-31 20:11:02 -0300

    esto es un tercer commit probando una rama nueva

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ console.log("esto es una prueba");
 console.log(moment().format("Do MMM YYYY"));
 console.log("meti un cambio");
 console.log("meti un segundo cambio"); //31/5/2021
+
+let fs = require("fs");
+
+let a = fs.readFileSync("./hola.txt","utf-8");
+console.log(a);
